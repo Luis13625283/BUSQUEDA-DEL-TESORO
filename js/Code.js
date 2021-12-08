1700001a2ef4ce467bbfef0db579c1cbc592c6c1
Luis13625283 <90009089+Luis13625283@users.noreply.github.com>
2021-12-08 17:13:39 -0600

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(20)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Pista: ¡siempre cambiante , no constante !!", 100,80)
+// add code for displaying rest of the names and the hints.
+    
+    fill("white")
+    textSize(20)
+    text("C U T N I F O N", 650,150)
+    fill("lightblue")
+    text("Pista: ¡realiza una tarea en particular  !!", 650,180)
+
+    fill("white")
+    textSize(20)
+    text("A T E D A S B A", 100,250)
+    fill("lightblue")
+    text("Pista: ¡Almacena toda la informacion !!", 100,280)
+}
